4ca7da969eee7ec758be5f4800880691b442b998
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2022-10-16 14:05:10 +0530

    Adds just command shell aliases

--- home.nix
+++ home.nix
@@ -8,7 +8,12 @@
   home.stateVersion = "22.05";
   home.enableNixpkgsReleaseCheck = true;
 
-  home.shellAliases = {
+  home.shellAliases =
+  let
+    projectsDir = "${config.home.homeDirectory}/Projects";
+    mm = "${projectsDir}/nix-managed-macbook";
+    nv = "${projectsDir}/nixed-DO-VPS";
+  in {
     g = "${pkgs.git}/bin/git";
     j = "${pkgs.just}/bin/just";
     l = "${pkgs.bat}/bin/bat";
@@ -17,7 +22,14 @@
     br = "${pkgs.broot}/bin/broot";
     du = "${pkgs.dua}/bin/dua interactive";
     tf = "${pkgs.coreutils-full}/bin/tail -f";
-    cat = "bat";
+    cat = "${pkgs.bat}/bin/bat";
+
+    home-manager-switch = "just ${mm}/switch";
+    home-manager-update = "just ${mm}/update";
+    home-manager-clean = "just ${mm}/clean";
+    vps-connect = "just ${nv}/connect";
+    vps-upgrade = "just ${nv}/upgrade";
+    deploy-website = "just ${nv}/run-service abhinavsarkar.net";
   };
 
   home.sessionVariables = { EDITOR = "micro"; };
