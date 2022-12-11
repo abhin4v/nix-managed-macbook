0449378572311d86634925aed37801f8f8fe967c
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2022-12-11 10:40:06 +0530

    Adds better defaults for nix settings

--- nix.nix
+++ nix.nix
@@ -8,5 +8,12 @@
     max-jobs = 6;
     cores = 2;
     auto-optimise-store = true;
+    connect-timeout = 5;
+    log-lines = 25;
+    min-free = 128000000;
+    max-free = 1000000000;
+    fallback = true;
+    warn-dirty = false;
+    keep-outputs = true;
   };
 }
