2a9711077267ab1445e49348563f64d0c3845343
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2023-11-24 16:35:04 +0530

    Fixes linter warnings

--- home.nix
+++ home.nix
@@ -3,42 +3,44 @@
 {
   imports = [ ./programs ./scripts ./launchd.nix ./nix.nix ];
 
-  home.username = "abhinav";
-  home.homeDirectory = "/Users/abhinav";
-  home.stateVersion = "22.05";
-  home.enableNixpkgsReleaseCheck = true;
+  home = {
+    username = "abhinav";
+    homeDirectory = "/Users/abhinav";
+    stateVersion = "22.05";
+    enableNixpkgsReleaseCheck = true;
 
-  home.shellAliases =
-  let
-    projectsDir = "${config.home.homeDirectory}/Projects";
-    mm = "${projectsDir}/nix-managed-macbook";
-    nv = "${projectsDir}/nixed-DO-VPS";
-  in {
-    g = "${pkgs.git}/bin/git";
-    j = "${pkgs.just}/bin/just";
-    l = "${pkgs.bat}/bin/bat";
-    m = "${pkgs.micro}/bin/micro";
+    shellAliases =
+    let
+      projectsDir = "${config.home.homeDirectory}/Projects";
+      mm = "${projectsDir}/nix-managed-macbook";
+      nv = "${projectsDir}/nixed-DO-VPS";
+    in {
+      g = "${pkgs.git}/bin/git";
+      j = "${pkgs.just}/bin/just";
+      l = "${pkgs.bat}/bin/bat";
+      m = "${pkgs.micro}/bin/micro";
 
-    br = "${pkgs.broot}/bin/broot";
-    du = "${pkgs.dua}/bin/dua interactive";
-    tf = "${pkgs.coreutils-full}/bin/tail -f";
-    cat = "${pkgs.bat}/bin/bat";
+      br = "${pkgs.broot}/bin/broot";
+      du = "${pkgs.dua}/bin/dua interactive";
+      tf = "${pkgs.coreutils-full}/bin/tail -f";
+      cat = "${pkgs.bat}/bin/bat";
 
-    home-manager-switch = "just ${mm}/switch";
-    home-manager-update = "just ${mm}/update";
-    home-manager-clean = "just ${mm}/clean";
-    vps-connect = "just ${nv}/connect";
-    vps-upgrade = "just ${nv}/upgrade";
-    deploy-website = "just ${nv}/run-service abhinavsarkar.net";
-  };
-
-  home.sessionVariables = { EDITOR = "micro"; };
-
-  home.file."Applications/Home Manager Apps".source = let
-    apps = pkgs.buildEnv {
-      name = "home-manager-applications";
-      paths = config.home.packages;
-      pathsToLink = "/Applications";
+      home-manager-switch = "just ${mm}/switch";
+      home-manager-update = "just ${mm}/update";
+      home-manager-clean = "just ${mm}/clean";
+      vps-connect = "just ${nv}/connect";
+      vps-upgrade = "just ${nv}/upgrade";
+      deploy-website = "just ${nv}/run-service abhinavsarkar.net";
     };
-  in "${apps}/Applications";
+
+    sessionVariables = { EDITOR = "micro"; };
+
+    file."Applications/Home Manager Apps".source = let
+      apps = pkgs.buildEnv {
+        name = "home-manager-applications";
+        paths = config.home.packages;
+        pathsToLink = "/Applications";
+      };
+    in "${apps}/Applications";
+  };
 }
--- programs/default.nix
+++ programs/default.nix
@@ -63,69 +63,71 @@ in {
 
   home.packages = nixPackages ++ networkingPackages ++ cmdLineUtilPackages ++ miscPackages ++ fonts;
 
-  programs.htop = {
-    enable = true;
-    settings = {
-      hide_kernel_threads = true;
-      hide_threads = true;
-      hide_userland_threads = true;
-      highlight_base_name = true;
-      show_program_path = false;
-      sort_direction = false;
-      sort_key = "PERCENT_CPU";
-      tree_view = true;
+  programs = {
+    htop = {
+      enable = true;
+      settings = {
+        hide_kernel_threads = true;
+        hide_threads = true;
+        hide_userland_threads = true;
+        highlight_base_name = true;
+        show_program_path = false;
+        sort_direction = false;
+        sort_key = "PERCENT_CPU";
+        tree_view = true;
+      };
     };
-  };
 
-  programs.eza = {
-    enable = true;
-    enableAliases = true;
-  };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv = { enable = true; };
-  };
-
-  programs.nix-index = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
-  programs.bat = {
-    enable = true;
-    config = {
-      italic-text = "always";
-      paging = "always";
-      tabs = "2";
-      theme = "DarkNeon";
+    eza = {
+      enable = true;
+      enableAliases = true;
     };
-  };
 
-  programs.fzf = {
-    enable = true;
-    enableFishIntegration = true;
-    fileWidgetCommand = "fd --type f --no-ignore";
-    historyWidgetOptions = [ "--reverse" "--sort" "--exact" ];
-  };
-
-  programs.micro = {
-    enable = true;
-    settings = {
-      autoindent = true;
-      colorcolumn = 100;
-      colorscheme = "gruvbox";
-      diffgutter = true;
-      hlsearch = true;
-      mkparents = true;
-      savecursor = true;
-      softwrap = true;
-      tabsize = 2;
-      tabstospaces = true;
-      # plugins
-      manipulator = true;
+    direnv = {
+      enable = true;
+      nix-direnv = { enable = true; };
     };
-  };
 
-  programs.zoxide.enable = true;
+    nix-index = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    bat = {
+      enable = true;
+      config = {
+        italic-text = "always";
+        paging = "always";
+        tabs = "2";
+        theme = "DarkNeon";
+      };
+    };
+
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+      fileWidgetCommand = "fd --type f --no-ignore";
+      historyWidgetOptions = [ "--reverse" "--sort" "--exact" ];
+    };
+
+    micro = {
+      enable = true;
+      settings = {
+        autoindent = true;
+        colorcolumn = 100;
+        colorscheme = "gruvbox";
+        diffgutter = true;
+        hlsearch = true;
+        mkparents = true;
+        savecursor = true;
+        softwrap = true;
+        tabsize = 2;
+        tabstospaces = true;
+        # plugins
+        manipulator = true;
+      };
+    };
+
+    zoxide.enable = true;
+  };
 }
