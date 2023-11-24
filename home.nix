8b569fd2c85ba8b848788226e44d4ce4bcecf53e
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2023-11-24 16:35:53 +0530

    Removes unused aliases

--- home.nix
+++ home.nix
@@ -9,12 +9,7 @@
     stateVersion = "22.05";
     enableNixpkgsReleaseCheck = true;
 
-    shellAliases =
-    let
-      projectsDir = "${config.home.homeDirectory}/Projects";
-      mm = "${projectsDir}/nix-managed-macbook";
-      nv = "${projectsDir}/nixed-DO-VPS";
-    in {
+    shellAliases = {
       g = "${pkgs.git}/bin/git";
       j = "${pkgs.just}/bin/just";
       l = "${pkgs.bat}/bin/bat";
@@ -24,13 +19,6 @@
       du = "${pkgs.dua}/bin/dua interactive";
       tf = "${pkgs.coreutils-full}/bin/tail -f";
       cat = "${pkgs.bat}/bin/bat";
-
-      home-manager-switch = "just ${mm}/switch";
-      home-manager-update = "just ${mm}/update";
-      home-manager-clean = "just ${mm}/clean";
-      vps-connect = "just ${nv}/connect";
-      vps-upgrade = "just ${nv}/upgrade";
-      deploy-website = "just ${nv}/run-service abhinavsarkar.net";
     };
 
     sessionVariables = { EDITOR = "micro"; };
