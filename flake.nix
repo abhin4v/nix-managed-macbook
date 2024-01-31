0d4776105ab2a62ba7a2dd0c906e8daf33eab7b4
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2024-01-31 16:02:50 +0530

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/3df2a80f3f85f91ea06e5e91071fa74ba92e5084' (2024-01-23)
      → 'github:nix-community/home-manager/d634c3abafa454551f2083b054cd95c3f287be61' (2024-01-28)
    • Updated input 'nix-darwin':
        'github:LnL7/nix-darwin/4b9b83d5a92e8c1fbfd8eb27eda375908c11ec4d' (2023-11-24)
      → 'github:LnL7/nix-darwin/91b9daf672c957ef95a05491a75f62e6a01d5aaf' (2024-01-30)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/5f5210aa20e343b7e35f40c033000db0ef80d7b9' (2024-01-22)
      → 'github:nixos/nixpkgs/222c1940fafeda4dea161858ffe6ebfc853d3db5' (2024-01-30)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix Darwin + Home Manager configuration of Abhinav Sarkar";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?rev=5f5210aa20e343b7e35f40c033000db0ef80d7b9";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
