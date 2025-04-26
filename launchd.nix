2a68404b976af65832d83429106d061f952cca4a
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2025-04-26 13:49:57 +0530

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/98f4fef7fd7b4a77245db12e33616023162bc6d9' (2025-04-25)
      → 'github:nix-community/home-manager/4d2d32231797bfa7213ae5e8ac89d25f8caaae82' (2025-04-26)
    • Updated input 'nix-vscode-extensions':
        'github:nix-community/nix-vscode-extensions/baeaec5a10fb8626bea64ebabdfaecdf64832bf3' (2025-04-25)
      → 'github:nix-community/nix-vscode-extensions/3ce03dadc2c5976a8f0f08c12d1e391006d7fa55' (2025-04-26)

--- launchd.nix
+++ launchd.nix
@@ -6,12 +6,21 @@
 }:
 
 let
+  cloudflare = pkgs.python3.pkgs.cloudflare.overrideAttrs (oldAttrs: {
+    patches = [ ./packages/python-cloudflare/hatchling.patch ];
+  });
+  pkg = (pkgs.cloudflare-dyndns).overridePythonAttrs (old: {
+    dependencies = (lib.lists.remove pkgs.python3.pkgs.cloudflare old.dependencies) ++ [
+      cloudflare
+    ];
+  });
+  # pkgs = pkgs-python-cloudflare-4_0_0;
   binPath = "${pkgs.coreutils-full}/bin";
   dyndns-updater-script = pkgs.writeScript "dyndns-updater" ''
     #!${pkgs.bash}/bin/bash
     export CLOUDFLARE_API_TOKEN=`${binPath}/cat ${config.xdg.configHome}/.cloudflare_dns_updater_api_token`
     ${binPath}/echo && ${binPath}/date
-    ${pkgs.cloudflare-dyndns}/bin/cloudflare-dyndns --debug home.abhinavsarkar.net
+    ${pkg}/bin/cloudflare-dyndns --debug home.abhinavsarkar.net
   '';
 in
 {
