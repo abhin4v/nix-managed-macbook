5bf1a8540fc79deb3b6fd9c58b7526af7ccaf35b
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2025-06-25 15:12:26 +0530

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/863842639722dd12ae9e37ca83bcb61a63b36f6c' (2025-06-19)
      → 'github:nix-community/home-manager/ff31a4677c1a8ae506aa7e003a3dba08cb203f82' (2025-06-24)
    • Updated input 'lix-module':
        'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/cd2a9c028df820a83ca2807dc6c6e7abc3dfa7fc.tar.gz?narHash=sha256-11R4K3iAx4tLXjUs%2BhQ5K90JwDABD/XHhsM9nkeS5N8%3D&rev=cd2a9c028df820a83ca2807dc6c6e7abc3dfa7fc' (2025-05-10)
      → 'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/c3c78a32273e89d28367d8605a4c880f0b6607e3.tar.gz?narHash=sha256-EfA5K5EZAnspmraJrXQlziffVpaT%2BQDBiE6yKmuaNNQ%3D&rev=c3c78a32273e89d28367d8605a4c880f0b6607e3' (2025-06-24)
    • Updated input 'lix-module/lix':
        'https://git.lix.systems/api/v1/repos/lix-project/lix/archive/47aad376c87e2e65967f17099277428e4b3f8e5a.tar.gz?narHash=sha256-hsFe4Tsqqg4l%2BFfQWphDtjC79WzNCZbEFhHI8j2KJzw%3D&rev=47aad376c87e2e65967f17099277428e4b3f8e5a' (2025-05-09)
      → 'https://git.lix.systems/api/v1/repos/lix-project/lix/archive/38b358ce27203f972faa2973cf44ba80c758f46e.tar.gz?narHash=sha256-LmQhjQ7c%2BAOkwhvR9GFgJOy8oHW35MoQRELtrwyVnPw%3D&rev=38b358ce27203f972faa2973cf44ba80c758f46e' (2025-06-24)
    • Updated input 'nix-darwin':
        'github:LnL7/nix-darwin/2f140d6ac8840c6089163fb43ba95220c230f22b' (2025-06-14)
      → 'github:LnL7/nix-darwin/1dd19f19e4b53a1fd2e8e738a08dd5fe635ec7e5' (2025-06-22)
    • Updated input 'nix-index-database':
        'github:nix-community/nix-index-database/424a40050cdc5f494ec45e46462d288f08c64475' (2025-06-15)
      → 'github:nix-community/nix-index-database/78cd697acc2e492b4e92822a4913ffad279c20e6' (2025-06-22)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/5395fb3ab3f97b9b7abca147249fa2e8ed27b192' (2025-06-18)
      → 'github:nixos/nixpkgs/69dfebb3d175bde602f612915c5576a41b18486b' (2025-06-24)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/8f49bca3dc47f48ef46511613450364fd82b0b36' (2025-06-17)
      → 'github:nixos/nixpkgs/a5e9291e97f5ba0b4ba7d657ddedd5f86d11acfd' (2025-06-24)

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
